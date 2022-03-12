72346c5d41a145d1a871751930e76d90cc79e576
raiyan24r <raiyan24r@gmail.com>
2022-03-12 19:54:17 +0600

    multiple json files and copy cut files og

---
+++ copy-lane-image.py
@@ -0,0 +1,17 @@
+import json
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import shutil
+directory = r'H:\Thesis 400\1.Dataset_Creation\Final\cut-visual'
+og = r'H:\Thesis 400\1.Dataset_Creation\lane_frames'
+dest = r'H:\Thesis 400\1.Dataset_Creation\Final\cut-og'
+
+for filename in os.scandir(directory):
+    if filename.is_file():
+        print(str(filename.name).split('_main')[0])
+        img_name = str(filename.name).split('_main')[0]
+        shutil.copy2(og+'\\'+img_name, dest)
+
+        # break
--- main.py
+++ main.py
@@ -1,50 +1,71 @@
 import json
+import os
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-f = open(r'annotation-json\250-320.json')
-data = json.load(f)
+import shutil
 
+# assign directory
+directory = r'H:\Thesis 400\1.Dataset_Creation\Final\annotations'
+thickness = 7
 
-for k in data.keys():
+# iterate over files in
+# that directory
+for filename in os.scandir(directory):
+    if filename.is_file():
+        print(filename.path)
+        annotation_file = filename.path
+        f = open(filename.path)
 
-    filename = data[k]['filename']
-    print(filename)
-    gt_lanes = data[k]['regions']
-    img = plt.imread(
-        r'H:\Thesis 400\1.Dataset_Creation\lane_frames\\'+filename)
-    height, width, channel = img.shape
+        data = json.load(f)
 
-    #!Get all co-ordinates
-    gt_lanes_vis = []
-    for lane in gt_lanes:
-        x_cor = lane['shape_attributes']['all_points_x']
-        y_cor = lane['shape_attributes']['all_points_y']
-        gt_lanes_vis.append([(x, y) for (x, y) in zip(x_cor, y_cor)])
+        for k in data.keys():
 
-    img_vis = img.copy()
-    #!Marking the exact co-ordinates
-    for lane in gt_lanes_vis:
-        for pt in lane:
-            cv2.circle(img_vis, pt, radius=5, color=(255, 255, 255))
+            filename = data[k]['filename']
+            print(filename)
+            gt_lanes = data[k]['regions']
+            img = plt.imread(
+                r'H:\Thesis 400\1.Dataset_Creation\lane_frames\\'+filename)
+            img = cv2.imread(
+                r'H:\Thesis 400\1.Dataset_Creation\lane_frames\\'+filename)
+            height, width, channel = img.shape
 
-    img_vis = img.copy()
-    #!Drawing polylines on top of the image
-    for lane in gt_lanes_vis:
-        cv2.polylines(img_vis, np.int32(
-            [lane]), isClosed=False, color=(0, 255, 0), thickness=5)
-    plt.imshow(img_vis)
-    cv2.imwrite('output\\'+filename+'_main.png', img_vis)
-    # plt.show()
+            #!Get all co-ordinates
+            gt_lanes_vis = []
+            for lane in gt_lanes:
+                x_cor = lane['shape_attributes']['all_points_x']
+                y_cor = lane['shape_attributes']['all_points_y']
+                gt_lanes_vis.append([(x, y) for (x, y) in zip(x_cor, y_cor)])
 
-    #!Generating gt image
-    gray_level = 0
-    gray_image = gray_level * np.ones((height, width, 3), dtype=np.uint8)
-    for lane in gt_lanes_vis:
-        cv2.polylines(gray_image, np.int32(
-            [lane]), isClosed=False, color=(255, 255, 255), thickness=5)
-    # plt.imshow(gray_image)
-    # cv2.imwrite('output\\'+filename+'_gt.png', gray_image)
-    # plt.show()
+            img_vis = img.copy()
+            #!Marking the exact co-ordinates
+            for lane in gt_lanes_vis:
+                for pt in lane:
+                    cv2.circle(img_vis, pt, radius=20, color=(255, 255, 255))
+
+            img_vis = img.copy()
+            #!Drawing polylines on top of the image
+            for lane in gt_lanes_vis:
+                cv2.polylines(img_vis, np.int32(
+                    [lane]), isClosed=False, color=(0, 255, 0), thickness=thickness)
+            plt.imshow(img_vis)
+            cv2.imwrite(
+                r'H:\Thesis 400\1.Dataset_Creation\Final\visual\\'+filename+'_main.png', img_vis)
+            # plt.show()
+
+            #!Generating gt image
+            gray_level = 0
+            gray_image = gray_level * \
+                np.ones((height, width, 3), dtype=np.uint8)
+            for lane in gt_lanes_vis:
+                cv2.polylines(gray_image, np.int32(
+                    [lane]), isClosed=False, color=(255, 255, 255), thickness=thickness)
+            plt.imshow(gray_image)
+            cv2.imwrite(r'H:\Thesis 400\1.Dataset_Creation\Final\gt\\' +
+                        filename+'_gt.png', gray_image)
+
+        f.close()
+
+        os.remove(annotation_file)
 
     # break
